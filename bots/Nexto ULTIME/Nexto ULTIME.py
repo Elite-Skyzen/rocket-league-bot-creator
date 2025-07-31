31c42f037104d6371b41b5b2cfda8a762f43dea8
Elite-Skyzen <tonemail@example.com>
2025-07-31 21:28:32 +0200

    Version web du site pour GitHub Pages

--- bots/Nexto ULTIME/Nexto ULTIME.py
+++
@@ -1,6 +0,0 @@
-
-from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
-
-class Nexto ULTIME(BaseAgent):
-    def get_output(self, packet):
-        return SimpleControllerState(throttle=1.0)
